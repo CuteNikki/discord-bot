9baca7fd8877355bf93d434b433c3f551d803687
Nikki <verschlafenes@gmail.com>
2024-04-29 19:00:09 +0200

    Rewrote commands class

--- src/classes/command.ts
+++ src/classes/command.ts
@@ -1,19 +1,16 @@
 import type { DiscordClient } from 'classes/client';
 import type { ApplicationCommandData, ChatInputCommandInteraction, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction } from 'discord.js';
 
-export type CommandOptions = {
-  developerOnly?: boolean; // If command is for developer only, it cannot be used by anyone else
-  cooldown?: number; // Cooldown between command executes per user (in milliseconds)
-  data: ApplicationCommandData;
-  execute: (options: {
-    client: DiscordClient;
-    interaction: ChatInputCommandInteraction | MessageContextMenuCommandInteraction | UserContextMenuCommandInteraction;
-  }) => any;
-};
-
 export class Command {
-  options: CommandOptions;
-  constructor(options: CommandOptions) {
-    this.options = options;
-  }
+  constructor(
+    public options: {
+      developerOnly?: boolean; // If command is for developer only, it cannot be used by anyone else
+      cooldown?: number; // Cooldown between command executes per user (in milliseconds)
+      data: ApplicationCommandData;
+      execute: (options: {
+        client: DiscordClient;
+        interaction: ChatInputCommandInteraction | MessageContextMenuCommandInteraction | UserContextMenuCommandInteraction;
+      }) => any;
+    }
+  ) {}
 }

0f67a63da485c98030b50fd6e74fa4b552922c66
Nikki <bravonikki13@gmail.com>
2024-07-31 16:19:31 +0200

    Updated ready event to use Events from discord

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,9 +1,11 @@
+import { Events } from 'discord.js';
+
 import { Event } from 'classes/event';
 
 import { logger } from 'utils/logger';
 
 export default new Event({
-  name: 'ready',
+  name: Events.ClientReady,
   once: true,
   execute(client, readyClient) {
     logger.info(`[${client.cluster.id}] Ready as ${readyClient.user.username}#${readyClient.user.discriminator}`);

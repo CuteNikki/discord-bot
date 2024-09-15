6b5754f0da997a956c4d3f43b5653d39333b8c7d
Nikki <bravonikki13@gmail.com>
2024-09-15 12:38:30 +0200

    Updated farewell event

--- src/events/farewell/guildMemberRemove.ts
+++ src/events/farewell/guildMemberRemove.ts
@@ -1,6 +1,6 @@
-import { EmbedBuilder, Events, type ColorResolvable } from 'discord.js';
+import { Events } from 'discord.js';
 
-import { replacePlaceholders } from 'classes/custom-embed';
+import { getEmbed, replacePlaceholders } from 'classes/custom-embed';
 import { Event } from 'classes/event';
 
 import { logger } from 'utils/logger';
@@ -9,46 +9,18 @@ export default new Event({
   name: Events.GuildMemberRemove,
   once: false,
   async execute(client, member) {
-    const { guild, user } = member;
-    if (user.bot) return;
+    if (member.user.bot) return;
 
-    const config = await client.getGuildSettings(guild.id);
+    const config = await client.getGuildSettings(member.guild.id);
 
     if (!config.farewell.enabled || !config.farewell.channelId) return;
-    const farewellChannel = await guild.channels.fetch(config.farewell.channelId);
+    const farewellChannel = await member.guild.channels.fetch(config.farewell.channelId);
     if (!farewellChannel?.isSendable()) return;
-
-    const embedData = config.farewell.message.embed;
 
     await farewellChannel
       .send({
-        content: replacePlaceholders(config.farewell.message.content ?? '', user, guild),
-        embeds: [
-          EmbedBuilder.from({
-            title: replacePlaceholders(embedData.title ?? '', user, guild),
-            author: {
-              name: replacePlaceholders(embedData.author?.name ?? '', user, guild),
-              icon_url: replacePlaceholders(embedData.author?.icon_url ?? '', user, guild),
-              url: replacePlaceholders(embedData.author.url ?? '', user, guild),
-            },
-            description: replacePlaceholders(embedData.description ?? '', user, guild),
-            fields: embedData.fields.map((field) => ({
-              name: replacePlaceholders(field.name, user, guild),
-              value: replacePlaceholders(field.value ?? '', user, guild),
-            })),
-            footer: {
-              text: replacePlaceholders(embedData.footer?.text ?? '', user, guild),
-              icon_url: replacePlaceholders(embedData.footer?.icon_url ?? '', user, guild),
-            },
-            image: {
-              url: replacePlaceholders(embedData.image ?? '', user, guild),
-            },
-            thumbnail: {
-              url: replacePlaceholders(embedData.thumbnail ?? '', user, guild),
-            },
-            url: replacePlaceholders(embedData.url ?? '', user, guild),
-          }).setColor(embedData.color as ColorResolvable),
-        ],
+        content: replacePlaceholders(config.farewell.message.content ?? '', member.user, member.guild),
+        embeds: [getEmbed(member.user, member.guild, config.farewell.message.embed)],
       })
       .catch((err) => logger.debug({ err }, 'Could not send farewell message'));
   },

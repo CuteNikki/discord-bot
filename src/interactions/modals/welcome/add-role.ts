50f77972db7374a084614478af870fd424995538
Nikki <bravonikki13@gmail.com>
2024-10-04 09:16:34 +0200

    Made code more readable

--- src/interactions/modals/welcome/add-role.ts
+++ src/interactions/modals/welcome/add-role.ts
@@ -10,20 +10,25 @@ export default new Modal({
   permissions: ['ManageGuild'],
   async execute({ client, interaction, lng }) {
     if (!interaction.inCachedGuild()) return;
+
     await interaction.deferReply();
 
     const roleId = interaction.fields.getTextInputValue('role-id');
+
     const config = await getGuildSettings(interaction.guild.id);
 
     if (config.welcome.roles.includes(roleId)) {
-      return interaction.editReply({
+      await interaction.editReply({
         embeds: [new EmbedBuilder().setColor(client.colors.error).setDescription(t('welcome.roles.already', { lng, roleId }))]
       });
+      return;
     }
+
     if (!interaction.guild.roles.cache.has(roleId)) {
-      return interaction.editReply({
+      await interaction.editReply({
         embeds: [new EmbedBuilder().setColor(client.colors.error).setDescription(t('welcome.roles.invalid', { lng, roleId }))]
       });
+      return;
     }
 
     await updateGuildSettings(interaction.guild.id, {
--- src/interactions/modals/welcome/remove-role.ts
+++ src/interactions/modals/welcome/remove-role.ts
@@ -10,13 +10,16 @@ export default new Modal({
   permissions: ['ManageGuild'],
   async execute({ client, interaction, lng }) {
     if (!interaction.inCachedGuild()) return;
+
     await interaction.deferReply();
 
     const roleId = interaction.fields.getTextInputValue('role-id');
+
     const config = await getGuildSettings(interaction.guild.id);
 
     if (!config.welcome.roles.includes(roleId)) {
-      return interaction.editReply({ embeds: [new EmbedBuilder().setColor(client.colors.error).setDescription(t('welcome.roles.none', { lng, roleId }))] });
+      await interaction.editReply({ embeds: [new EmbedBuilder().setColor(client.colors.error).setDescription(t('welcome.roles.none', { lng, roleId }))] });
+      return;
     }
 
     await updateGuildSettings(interaction.guild.id, {

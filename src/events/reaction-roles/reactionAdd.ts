4b9115458f0b4296e71c16be8ede5297d9964636
Nikki <bravonikki13@gmail.com>
2024-10-04 11:42:38 +0200

    Cleaned up

--- src/events/reaction-roles/reactionAdd.ts
+++ src/events/reaction-roles/reactionAdd.ts
@@ -8,24 +8,43 @@ export default new Event({
   name: Events.MessageReactionAdd,
   once: false,
   async execute(_client, reaction, user) {
-    if (!reaction.message.inGuild()) return;
+    if (!reaction.message.inGuild()) {
+      return;
+    }
 
     const config = await getGuildSettings(reaction.message.guildId);
-    if (!config.reactionRoles.enabled) return;
+
+    if (!config.reactionRoles.enabled) {
+      return;
+    }
 
     const group = config.reactionRoles.groups.find((g) => g.messageId === reaction.message.id);
-    if (!group) return;
+
+    if (!group) {
+      return;
+    }
 
     const reactionIndex = group.reactions.findIndex((r) => r.emoji === reaction.emoji.toString());
-    if (reactionIndex === -1) return;
+
+    if (reactionIndex === -1) {
+      return;
+    }
 
     const role = reaction.message.guild.roles.cache.get(group.reactions[reactionIndex].roleId);
-    if (!role) return;
+
+    if (!role) {
+      return;
+    }
 
     const member = reaction.message.guild.members.cache.get(user.id);
-    if (!member) return;
 
-    if (member.roles.cache.has(role.id)) return;
+    if (!member) {
+      return;
+    }
+
+    if (member.roles.cache.has(role.id)) {
+      return;
+    }
 
     await member.roles.add(role).catch((err) => console.error(err));
   }

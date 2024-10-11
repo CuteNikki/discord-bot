97d5d3b32be35793a0b700b81efe6c36bcb65081
Nikki <bravonikki13@gmail.com>
2024-10-11 10:32:12 +0200

    Adjusted eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import { includeIgnoreFile } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
@@ -7,6 +8,9 @@ import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
@@ -15,6 +19,7 @@ const compat = new FlatCompat({
 
 export default [
   ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'),
+  includeIgnoreFile(gitignorePath),
   {
     plugins: {
       '@typescript-eslint': typescriptEslint

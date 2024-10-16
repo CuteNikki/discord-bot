fdc1216920d85c1e7870662dea140f30798245b0
Nikki <bravonikki13@gmail.com>
2024-10-16 15:06:59 +0200

    Fixing welcome roles again...

--- src/interactions/commands/config/welcome.ts
+++ src/interactions/commands/config/welcome.ts
@@ -107,7 +107,7 @@ export default new Command({
               ? welcome.roles
                   .map((r) => {
                     const role = guild.roles.cache.get(r);
-                    return role ? role.toString() : role;
+                    return role ? role.toString() : r;
                   })
                   .join('\n')
               : t('none', { lng })

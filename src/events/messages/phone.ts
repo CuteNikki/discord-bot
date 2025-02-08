e43dee15d2a235e4ef0a4b50e4414e36a286a51a
Nikki <bravonikki13@gmail.com>
2025-02-08 19:19:24 +0100

    Adjusted link regex for phone

--- src/events/messages/phone.ts
+++ src/events/messages/phone.ts
@@ -46,9 +46,8 @@ export default new Event({
       .setDescription(message.content)
       .setTimestamp();
 
-    // Define a regex for detecting links
     const linkRegex =
-      /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+      /(?:https?:\/\/|www\.)[\w-]+\.\w{2,}(\/\S*)?|\b[\w-]+\.(?:com|net|org|xyz|moe|edu|gov|info|io|tech|ai|biz|co|me|dev|tv|cc|app|uk|us|ca|eu|fr|de|jp|cn)\b/gm;
     // Replace message content if it contains a link
     if (linkRegex.test(message.content)) {
       embed.setDescription(`||${message.content}||`).setFooter({

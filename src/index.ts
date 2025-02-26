1d3b81ea05a585c8f215eac7bbade268646f9a7e
Nikki <bravonikki13@gmail.com>
2025-02-26 19:06:26 +0100

    Update logging level to debug

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,26 @@ const userId = '303142922780672013';
 
 const user = await getUser(userId, { banInfo: true }, true);
 
-logger.info({ data: user }, 'User before update');
+logger.debug({ data: user }, 'User before update');
 
 if (user?.banInfo) {
   const deletedBan = await unbanUser(userId);
-  logger.info({ deletedBan }, 'Deleted Ban');
+  logger.debug({ deletedBan }, 'Deleted Ban');
 } else {
   const createdBan = await banUser(userId, {
     moderatorId: userId,
     reason: 'Being a bad user',
     expiresAt: new Date(Date.now() + 10_000), // 10 seconds from now
   });
-  logger.info({ data: createdBan }, 'Created Ban');
-  logger.info({ data: Math.floor(createdBan.bannedAt.getTime() / 1000) }, 'Banned at');
+  logger.debug({ data: createdBan }, 'Created Ban');
+  logger.debug({ data: Math.floor(createdBan.bannedAt.getTime() / 1000) }, 'Banned at');
 }
 
 const updatedUser = await getUser(userId, { banInfo: true });
-logger.info({ data: updatedUser }, 'User after update');
+logger.debug({ data: updatedUser }, 'User after update');
 
 const bansByModerator = await getBansByModerator(userId, { user: true });
-logger.info({ data: bansByModerator }, 'Bans by Moderator');
+logger.debug({ data: bansByModerator }, 'Bans by Moderator');
 
 const bannedUsers = await getBans({ user: true });
-logger.info({ data: bannedUsers }, 'Banned users');
+logger.debug({ data: bannedUsers }, 'Banned users');

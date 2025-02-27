28d64e7aa2cea45c72ab6e0d6337894859308b48
Nikki <bravonikki13@gmail.com>
2025-02-27 09:33:46 +0100

    Added comments, removed unused cron schedule

--- src/database/cron.ts
+++ src/database/cron.ts
@@ -2,8 +2,28 @@ import cron from 'node-cron';
 
 import { deleteExpiredBans } from 'database/ban';
 
+/*
+*
+* Coming soon
+*
+
 // Run every day at midnight
-cron.schedule('0 0 * * *', () => {});
+cron.schedule('0 0 * * *', () => {
+  // coming soon... // Clear daily stats
+});
+
+// Run every week on Sunday at midnight
+cron.schedule('0 0 * * 0', () => {
+  // coming soon... // Clear weekly stats
+});
+
+*
+*
+*/
 
 // Run every minute
-cron.schedule('* * * * *', async () => await deleteExpiredBans());
+cron.schedule('* * * * *', async () => {
+  await deleteExpiredBans(); // Clear expired user bans
+  // coming soon... // Clear expired infractions
+  // coming soon... // Clear expired reminders
+});

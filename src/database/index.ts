679b94272e117b3c266b55dc38c0a022f5d26176
Nikki <bravonikki13@gmail.com>
2025-02-27 08:56:07 +0100

    Refactor logging

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,2 +1,28 @@
 import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
+
+import logger from 'utility/logger';
+
+export const prisma = new PrismaClient({
+  log: [
+    {
+      level: 'query',
+      emit: 'event',
+    },
+    {
+      level: 'info',
+      emit: 'stdout',
+    },
+    {
+      level: 'warn',
+      emit: 'stdout',
+    },
+    {
+      level: 'error',
+      emit: 'stdout',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  logger.debug(`Query: ${e.query} | Duration: ${e.duration}ms`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
+import logger from 'utility/logger';
+
 import { banUser, getBans, getBansByModerator, unbanUser } from 'database/ban';
 import { getUser } from 'database/user';
 
 import 'database/cron';
-import logger from './logger';
 
 const userId = '303142922780672013';
 
 const user = await getUser(userId, { banInfo: true }, true);
 
-logger.debug({ data: user }, 'User before update');
+logger.info({ data: user }, 'User before update');
 
 if (user?.banInfo) {
   const deletedBan = await unbanUser(userId);
-  logger.debug({ deletedBan }, 'Deleted Ban');
+  logger.info({ deletedBan }, 'Deleted Ban');
 } else {
   const createdBan = await banUser(userId, {
     moderatorId: userId,
     reason: 'Being a bad user',
     expiresAt: new Date(Date.now() + 10_000), // 10 seconds from now
   });
-  logger.debug({ data: createdBan }, 'Created Ban');
-  logger.debug({ data: Math.floor(createdBan.bannedAt.getTime() / 1000) }, 'Banned at');
+  logger.info({ data: createdBan }, 'Created Ban');
+  logger.info({ data: Math.floor(createdBan.bannedAt.getTime() / 1000) }, 'Banned at');
 }
 
 const updatedUser = await getUser(userId, { banInfo: true });
-logger.debug({ data: updatedUser }, 'User after update');
+logger.info({ data: updatedUser }, 'User after update');
 
 const bansByModerator = await getBansByModerator(userId, { user: true });
-logger.debug({ data: bansByModerator }, 'Bans by Moderator');
+logger.info({ data: bansByModerator }, 'Bans by Moderator');
 
 const bannedUsers = await getBans({ user: true });
-logger.debug({ data: bannedUsers }, 'Banned users');
+logger.info({ data: bannedUsers }, 'Banned users');

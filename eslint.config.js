53f4600bebab4954d026095ebf99313a06347de2
Nikki <bravonikki13@gmail.com>
2025-02-27 09:21:57 +0100

    Add eslint-config-prettier to improve ESLint and Prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -8,4 +9,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];

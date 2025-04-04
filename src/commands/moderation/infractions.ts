6257707771956defcf88e0c573e2a2c340516aef
Nikki <bravonikki13@gmail.com>
2025-04-04 12:33:58 +0200

    Improved infractions styles

--- src/commands/moderation/infractions.ts
+++ src/commands/moderation/infractions.ts
@@ -1,3 +1,4 @@
+import { InfractionType } from '@prisma/client';
 import {
   ActionRowBuilder,
   ApplicationIntegrationType,
@@ -15,6 +16,7 @@ import {
   TimestampStyles,
 } from 'discord.js';
 
+import type { ExtendedClient } from 'classes/client';
 import { Command } from 'classes/command';
 import { Pagination } from 'classes/pagination';
 
@@ -34,17 +36,30 @@ export default new Command({
       return;
     }
 
+    const client = interaction.client as ExtendedClient;
+
+    const staffEmoji = client.customEmojis.staff;
+    const dateEmoji = client.customEmojis.date;
+    const calendarEmoji = client.customEmojis.calendar;
+    const receiptEmoji = client.customEmojis.receipt;
+    const pencilEmoji = client.customEmojis.pencil;
+    const backwardsEmoji = client.customEmojis.backwards;
+    const forwardsEmoji = client.customEmojis.forwards;
+    const nextEmoji = client.customEmojis.right;
+    const previousEmoji = client.customEmojis.left;
+
     await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
 
     const targetUser = interaction.options.getUser('user', true);
 
     const ITEMS_PER_PAGE = 3;
 
+    const totalInfractions = await getInfractionsByUserIdAndGuildId(targetUser.id, interaction.guild.id);
+
     new Pagination({
       interaction: interaction,
       notFoundEmbed: new EmbedBuilder().setColor(Colors.Red).setDescription(`No infractions found for ${targetUser.toString()}!`),
       getTotalPages: async () => {
-        const totalInfractions = await getInfractionsByUserIdAndGuildId(targetUser.id, interaction.guild.id);
         return Math.ceil(totalInfractions.length / ITEMS_PER_PAGE);
       },
       getPageContent: async (pageIndex) => {
@@ -58,36 +73,63 @@ export default new Command({
 
         if (!pageInfractions.length) return null;
 
-        return new EmbedBuilder()
-          .setColor(Colors.DarkRed)
-          .setDescription(`## Infractions of ${targetUser.toString()}`)
-          .setThumbnail(targetUser.displayAvatarURL())
-          .addFields(
-            pageInfractions.map((infraction) => ({
-              name: `${infraction.id}`,
-              value: [
-                `Type: ${infraction.type}`,
-                `Reason: ${infraction.reason}`,
-                `Moderator: <@${infraction.moderatorId}>`,
-                `Date: ${time(Math.floor(infraction.createdAt.getTime() / 1_000), TimestampStyles.ShortDateTime)}`,
-                infraction.expiresAt &&
-                  `Expires: ${time(Math.floor(infraction.expiresAt.getTime() / 1_000), TimestampStyles.ShortDateTime)}`,
+        return [
+          new EmbedBuilder()
+            .setColor(Colors.White)
+            .setAuthor({ name: targetUser.displayName, iconURL: targetUser.displayAvatarURL() })
+            .setDescription(
+              [
+                `Total: ${totalInfractions.length}`,
+                `Bans: ${totalInfractions.filter((infraction) => infraction.type === InfractionType.Ban).length}`,
+                `Tempbans: ${totalInfractions.filter((infraction) => infraction.type === InfractionType.Tempban).length}`,
+                `Kicks: ${totalInfractions.filter((infraction) => infraction.type === InfractionType.Kick).length}`,
+                `Warns: ${totalInfractions.filter((infraction) => infraction.type === InfractionType.Warn).length}`,
+                `Timeouts: ${totalInfractions.filter((infraction) => infraction.type === InfractionType.Timeout).length}`,
               ].join('\n'),
-            })),
-          );
+            ),
+          // Map through the infractions and create an embed for each
+          ...pageInfractions.map((infraction) =>
+            infraction.expiresAt
+              ? new EmbedBuilder()
+                  .setColor(Colors.White)
+                  .setDescription(
+                    [
+                      `**${infraction.id}**`,
+                      '',
+                      `${receiptEmoji} Type: ${infraction.type}`,
+                      `${pencilEmoji} Reason: ${infraction.reason}`,
+                      `${staffEmoji} Moderator: <@${infraction.moderatorId}>`,
+                      `${calendarEmoji} Expires: ${time(Math.floor(infraction.expiresAt.getTime() / 1_000), TimestampStyles.ShortDateTime)}`,
+                      `${dateEmoji} Date: ${time(Math.floor(infraction.createdAt.getTime() / 1_000), TimestampStyles.ShortDateTime)}`,
+                    ].join('\n'),
+                  )
+              : new EmbedBuilder()
+                  .setColor(Colors.White)
+                  .setDescription(
+                    [
+                      `**${infraction.id}**`,
+                      '',
+                      `${receiptEmoji} Type: ${infraction.type}`,
+                      `${pencilEmoji} Reason: ${infraction.reason}`,
+                      `${staffEmoji} Moderator: <@${infraction.moderatorId}>`,
+                      `${dateEmoji} Date: ${time(Math.floor(infraction.createdAt.getTime() / 1_000), TimestampStyles.ShortDateTime)}`,
+                    ].join('\n'),
+                  ),
+          ),
+        ];
       },
       buttons: [
         // First page button
         () => ({
-          data: new ButtonBuilder().setCustomId('pagination_first').setStyle(ButtonStyle.Secondary).setEmoji({ name: '⏪' }),
-          disableOn: (index) => index === 0,
-          onClick: () => 0,
+          data: new ButtonBuilder().setCustomId('pagination_first').setStyle(ButtonStyle.Secondary).setEmoji({ id: backwardsEmoji.id }),
+          disableOn: (index) => index === 0, // Disable if on the first page
+          onClick: () => 0, // Go to the first page
         }),
         // Previous page button
         () => ({
-          data: new ButtonBuilder().setCustomId('pagination_previous').setStyle(ButtonStyle.Secondary).setEmoji({ name: '⬅️' }),
-          disableOn: (index) => index === 0,
-          onClick: (index) => (index > 0 ? index - 1 : index),
+          data: new ButtonBuilder().setCustomId('pagination_previous').setStyle(ButtonStyle.Secondary).setEmoji({ id: previousEmoji.id }),
+          disableOn: (index) => index === 0, // Disable if on the first page
+          onClick: (index) => (index > 0 ? index - 1 : index), // Go to the previous page
         }),
         // Custom page button
         (index, totalPages) => ({
@@ -156,15 +198,15 @@ export default new Command({
         }),
         // Next page button
         () => ({
-          data: new ButtonBuilder().setCustomId('pagination_next').setStyle(ButtonStyle.Secondary).setEmoji({ name: '➡️' }),
-          disableOn: (index, totalPages) => index === totalPages - 1,
-          onClick: (index, totalPages) => (index < totalPages - 1 ? index + 1 : index),
+          data: new ButtonBuilder().setCustomId('pagination_next').setStyle(ButtonStyle.Secondary).setEmoji({ id: nextEmoji.id }),
+          disableOn: (index, totalPages) => index === totalPages - 1, // Disable if on the last page
+          onClick: (index, totalPages) => (index < totalPages - 1 ? index + 1 : index), // Go to the next page
         }),
         // Last page button
         () => ({
-          data: new ButtonBuilder().setCustomId('pagination_last').setStyle(ButtonStyle.Secondary).setEmoji({ name: '⏩' }),
-          disableOn: (index, totalPages) => index === totalPages - 1,
-          onClick: (_index, totalPages) => totalPages - 1,
+          data: new ButtonBuilder().setCustomId('pagination_last').setStyle(ButtonStyle.Secondary).setEmoji({ id: forwardsEmoji.id }),
+          disableOn: (index, totalPages) => index === totalPages - 1, // Disable if on the last page
+          onClick: (_index, totalPages) => totalPages - 1, // Go to the last page
         }),
       ],
     });

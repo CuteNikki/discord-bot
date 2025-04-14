b4ded00b02b8a7b7ed7a75279375aac5685eb2e6
Nikki <bravonikki13@gmail.com>
2025-04-14 09:32:55 +0200

    Refactor getUser for improved type handling

--- src/database/user.ts
+++ src/database/user.ts
@@ -4,18 +4,19 @@ import { prisma } from 'database/index';
 
 export const getUsers = () => prisma.user.findMany();
 
-export const getUser = <T extends boolean>(userId: string, include: Prisma.UserInclude = {}, upsert: T = false as T) =>
-  upsert
-    ? prisma.user.upsert({
-        where: { userId },
-        update: {},
-        create: { userId },
-        include,
-      })
-    : prisma.user.findUnique({
-        where: { userId },
-        include,
-      });
+export const getUser = async <T extends Prisma.UserInclude>(userId: string, include?: T) =>
+  (await prisma.user.findUnique({
+    where: { userId },
+    include,
+  })) as Prisma.UserGetPayload<{ include: T }> | null;
+
+export const getUserOrCreate = async <T extends Prisma.UserInclude>(userId: string, include?: T) =>
+  (await prisma.user.upsert({
+    where: { userId },
+    update: {},
+    create: { userId },
+    include,
+  })) as Prisma.UserGetPayload<{ include: T }>;
 
 export const updateUser = (userId: string, data: Partial<Omit<User, 'userId'>>) =>
   prisma.user.upsert({
